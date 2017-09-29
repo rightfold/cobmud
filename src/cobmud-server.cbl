7e6a4623db45d797a58f116e835db3a7f5b4def0
rightfold <rightfold@gmail.com>
2017-09-29 13:05:18 +0200

    Receive incoming requests

--- src/cobmud-server.cbl
+++ src/cobmud-server.cbl
@@ -10,9 +10,13 @@
        01 zmq-ctx                       pointer.
        01 zmq-socket                    pointer.
 
+       01 zmq-request                   pic X(256).
+       01 zmq-recv-flags                binary-int value 0.
+       01 zmq-send-flags                binary-int value 0.
+
        01 zmq-errno                     binary-int.
        01 zmq-strerror                  pointer.
-       01 zmq-bind-ok                   binary-int.
+       01 zmq-ok                        binary-int.
 
        procedure division.
        main-para.
@@ -24,23 +28,41 @@
            call "zmq_ctx_new" giving zmq-ctx
            if zmq-ctx = null then perform error-para end-if
 
-           call "zmq_socket" using by value zmq-ctx
-                                   by value ZMQ_REP
+           call "zmq_socket" using value zmq-ctx
+                                   value ZMQ_REP
                              giving zmq-socket
            if zmq-socket = null then perform error-para end-if
 
-           call "zmq_bind" using by value zmq-socket
-                                 by reference bind-address
-                           giving zmq-bind-ok
-           if zmq-bind-ok = -1 then perform error-para end-if
+           call "zmq_bind" using value zmq-socket
+                                 reference bind-address
+                           giving zmq-ok
+           if zmq-ok = -1 then perform error-para end-if
+
+           perform forever
+               call "zmq_recv" using value zmq-socket
+                                     reference zmq-request
+                                     value function length(zmq-request)
+                                     value zmq-recv-flags
+                               giving zmq-ok
+               if zmq-ok = -1 then perform error-para end-if
+
+               call "zmq_send" using value zmq-socket
+                                     reference zmq-request
+                                     value function length(zmq-request)
+                                     value zmq-send-flags
+                               giving zmq-ok
+               if zmq-ok = -1 then perform error-para end-if
+
+               display zmq-request
+           end-perform
 
            goback
            .
 
        error-para.
            call "zmq_errno" giving zmq-errno
-           call "zmq_strerror" using by value zmq-errno
+           call "zmq_strerror" using value zmq-errno
                                giving zmq-strerror
-           call "puts" using by value zmq-strerror
+           call "puts" using value zmq-strerror
            goback giving 1
            .
